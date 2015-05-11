9228f4544005f954f72ee29c66d4d7e1e2c04b52
Roxana Masis <rroxana10@gmail.com>
2015-05-11 14:34:35 -0700

    Prueba git

--- Source/main.c
+++ Source/main.c
@@ -3,8 +3,8 @@
 void main(int argc, char *argv[])
 {
 	//Se esperan un parámetro para determinar la cantidad de líneas de memoria compartida.
-	if(argc == 2) 
-	{	
+	if(argc == 2) {
+		// Comentario para github
 		int cantidadLineas = atoi(argv[1]);
 		crearMemoria(cantidadLineas);			
 	}

34e2384b73b069f71a185f40c5aa496fc6b87ec5
ken <kennethms64@gmail.com>
2015-05-17 16:09:18 -0600

    se agregaron threads a los writers

--- Headers/writer.h
+++ Headers/writer.h
@@ -4,11 +4,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/shm.h>
+#include <pthread.h>
 #include "llaves.h"
 #include "inicializador.h"
 
-//Inicializa los writers, recibe cantidad de writers, tiempo de escritura
+typedef struct writer 
+{
+    int id;
+    int descanso;
+    int escritura;
+}writer;
+
+// Retorna un nuevo writer
+// recibe id, tiempo de escritura, tiempo de descanso
+writer* newWriter(int,int,int);
+
+// Inicializa los writers, recibe cantidad de writers, tiempo de escritura
 // y tiempo de descanso
 void initWriters(int,int,int);
+
+// Hilo encargado de la ejecución del writer
+void ejecutarWriter(writer*);
 
 #endif // WRITER_H_
--- Source/writer.c
+++ Source/writer.c
@@ -1,15 +1,34 @@
 #include "../Headers/writer.h"
 
 
-void writer(int ID){
-	printf("%d\n",ID);
+// Retorna un nuevo writer
+// recibe id, tiempo de escritura, tiempo de descanso
+writer* newWriter(int pId, int pEscritura,int pDescanso){
+	writer *newWriter = malloc(sizeof(writer));
+  	newWriter->id = pId;
+  	newWriter->descanso = pEscritura;
+  	newWriter->escritura = pDescanso;
+	return newWriter;
+}
+
+void ejecutarWriter(writer *writerActual){
+	printf("%d %d\n",writerActual->id,writerActual->escritura);
+	sleep(writerActual->escritura);
+	printf("%d %d\n",writerActual->id,writerActual->descanso);
+	sleep(writerActual->descanso);
+	printf("fin\n");
 }
 
 //Inicializa los writers, recibe cantidad de writers, tiempo de escritura
 // y tiempo de descanso
 void initWriters(int cantidadWriters,int tiempoEscritura,int tiempoDescanso){
 	int i;
+	pthread_t writerThread[cantidadWriters];
 	for(i=1;i<=cantidadWriters;i++){
-		writer(i);
+		writer *nWriter = newWriter(i,tiempoEscritura,tiempoDescanso);
+		pthread_create(&writerThread[i-1], NULL, ejecutarWriter, nWriter);
+	}
+	for(i=0;i<cantidadWriters;i++){
+		pthread_join(writerThread[i], NULL);
 	}
 }
